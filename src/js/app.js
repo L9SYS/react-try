a8d39a744ef9d5a8ef0a283fd5f1146dd71b12ab
Alexey Bubeev <alexey.bubeev@gmail.com>
2017-11-15 18:06:00 +0300

    1st upload

---
+++ src/js/app.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+import '../css/style.css';
+
+import sameImg from '../assets/sameImg.jpg';
+
+export default class Hello extends Component {
+    render() {
+        return (
+            <div>
+            Hello from react
+
+            <img src={ sameImg } alt='Danny Choo Hatsune Miku Dollfie' />
+        </div>
+    );
+    }
+}
+
+render(<Hello />, document.getElementById('app'));
