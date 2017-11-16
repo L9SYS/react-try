402f55f9337d85460fc933b12e1966ec193b43f0
Alexey Bubeev <alexey.bubeev@gmail.com>
2017-11-16 19:08:52 +0300

    try react

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,88 @@
-import React, { Component } from 'react';
-import { render } from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
-import '../style/style.scss';
+import '../style/style';
 
-import sameImg from '../assets/sameImg.jpg';
+// import sameImg from '../assets/sameImg.jpg';
+//
+// export default class Hello extends Component {
+//     render() {
+//         return (
+//             <div>
+//                 Hatsune Miku Dollfie by Danny Choo
+//                 <img src={ sameImg } alt='Hatsune Miku Dollfie by Danny Choo' />
+//             </div>
+//         );
+//     }
+// }
+// render(<Hello />, document.getElementById('app'));
 
-export default class Hello extends Component {
+class Square extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null,
+        };
+    }
+
     render() {
         return (
-            <div>
-            Hello from react
-            <img src={ sameImg } alt='Hatsune Miku Dollfie by Danny Choo' />
-        </div>
-    );
+            <button className="square" onClick={() => this.setState({value: 'X'})}>
+                {this.state.value}
+            </button>
+        );
     }
 }
 
-render(<Hello />, document.getElementById('app'));
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square value={i} />;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div>
+                <div className="status">{status}</div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
+
+class Game extends React.Component {
+    render() {
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board />
+                </div>
+                <div className="game-info">
+                    <div>{/* status */}</div>
+                    <ol>{/* TODO */}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Game />,
+    document.getElementById('app')
+);
+
